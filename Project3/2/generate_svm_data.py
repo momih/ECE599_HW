286935c7f3b074fd53b10760738bc2cc53359366
Hashir <mohammadhashir94@gmail.com>
2017-10-24 21:55:43 -0400

    added noise and dtype from sklearn

--- Project3/2/generate_svm_data.py
+++ Project3/2/generate_svm_data.py
@@ -2,6 +2,7 @@ import pickle
 import tensorflow as tf
 import sys
 sys.path.insert(0, '../utils/')
+import numpy as np
 
 import getdata
 from LeNet5 import LeNet
@@ -9,28 +10,38 @@ from LeNet5 import LeNet
 from autoencoder import DenoisingAutoencoder as DAE
 import datasets
 
-dae = DAE(model_name='dae_svm', pickle_name='svm', test_name='svm',
-             n_components=256, main_dir='dae/', 
-             enc_act_func='sigmoid', dec_act_func='none', 
-             loss_func='mean_squared', num_epochs=11, batch_size=20, 
-             dataset='cifar10', xavier_init=1, opt='momentum', 
-             learning_rate=0.001, momentum=0.5, corr_type='gaussian',
-             corr_frac=0.3, verbose=1, seed=1)    
+if sys.argv[1] =='dae':
+    dae = DAE(model_name='dae_svm', pickle_name='svm', test_name='svm',
+                 n_components=256, main_dir='dae/', 
+                 enc_act_func='sigmoid', dec_act_func='none', 
+                 loss_func='mean_squared', num_epochs=6, batch_size=20, 
+                 dataset='cifar10', xavier_init=1, opt='momentum', 
+                 learning_rate=0.001, momentum=0.5, corr_type='gaussian',
+                 corr_frac=0.3, verbose=1, seed=1)    
+    
+    trX, trY, teX, teY = datasets.load_cifar10_dataset('../cifar-10-batches-py/', mode='supervised')
+    val_dict = {}
+    dae.fit(trX, val_dict, teX, restore_previous_model=True) 
+    
+    #dae.load_model(256, 'models/dae/dae_svm')
+    dae_svm_train = dae.transform(trX, name='dae_svm_train', save=True)
+    dae_svm_test = dae.transform(teX, name='dae_svm_test', save=True)
 
-trX, trY, teX, teY = datasets.load_cifar10_dataset('../cifar-10-batches-py/', mode='supervised')
-val_dict = {}
-dae.fit(trX, val_dict, teX, restore_previous_model=True) 
-
-#dae.load_model(256, 'models/dae/dae_svm')
-dae_svm_data = dae.transform(trX, name='dae_svm', save=True)
-
-
-cifar_train = getdata.get_train()
-cifar_test = getdata.get_test()
-acc_list = []
-cnn = LeNet(lr=0.001, epochs=101, batch_size=256, train_data=cifar_train,
-                     test_data=cifar_test, wd=0.004, decay_lr=False,
-                     decay_w=False, optimizer='rmsprop', seed=124, 
-                     model_name='lenet/tests', init_dev=0.01, drop=0.2)
-cnn.train(acc_list)
-cnn.transform(cifar_train.images[:100], name='cnn', save=True)
+    
+elif sys.argv[1]=='cnn':
+    cifar_train = getdata.get_train()
+    cifar_test = getdata.get_test()
+    acc_list = []
+    cnn = LeNet(lr=0.001, epochs=101, batch_size=256, train_data=cifar_train,
+                         test_data=cifar_test, wd=0.004, decay_lr=False,
+                         decay_w=False, optimizer='rmsprop', seed=124, 
+                         model_name='lenet/rmsprop', init_dev=0.01, drop=0.2)
+    cnn.restore('models/lenet/rmsprop')
+    train_list = np.split(cifar_train.images, 20)
+    test_list = np.split(cifar_test.images, 4)
+    for i in range(len(train_list)):
+        print "\n Batch" + str(i)
+        cnn.transform(train_list[i], name='cnn_train' + str(i), save=True)
+    for i in range(len(test_list)):
+        print "\n Batch" + str(i)
+        cnn.transform(test_list[i], name='cnn_test' + str(i), save=True)
--- Project3/utils/dae_utils.py
+++ Project3/utils/dae_utils.py
@@ -1,7 +1,7 @@
 from scipy import misc
 import tensorflow as tf
 import numpy as np
-from skimage.util import random_noise
+from noise import random_noise
 
 # ############# #
 #   Utilities   #
