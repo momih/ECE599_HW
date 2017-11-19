b051b05e76b4767b589b822523e99e60caf8df5c
Hashir <mohammadhashir94@gmail.com>
2017-11-19 00:20:39 -0500

    4

--- Project4/download.py
+++ Project4/download.py
@@ -58,8 +58,7 @@ def get_confirm_token(response):
 def save_response_content(response, destination, chunk_size=32*1024):
   total_size = int(response.headers.get('content-length', 0))
   with open(destination, "wb") as f:
-    for chunk in response.iter_content(chunk_size), total=total_size,
-              unit='B', unit_scale=True, desc=destination:
+    for chunk in response:
       if chunk: # filter out keep-alive new chunks
         f.write(chunk)
 
