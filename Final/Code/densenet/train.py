90afd9e8da9e7e96070992f3aac892f744804bab
Hashir <mohammadhashir94@gmail.com>
2017-11-21 14:36:57 -0500

    split_data

--- Final/Code/densenet/train.py
+++ Final/Code/densenet/train.py
@@ -1,7 +1,6 @@
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, CSVLogger
 from keras.preprocessing.image import ImageDataGenerator
-from sklearn.metrics import accuracy_score, log_loss
-from sklearn.model_selection import train_test_split
+#from sklearn.metrics import accuracy_score, log_loss
 from densenet121 import densenet
 import numpy as np
 import os
@@ -9,55 +8,71 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--epochs', action="store", dest="epochs", default=50)
-parser.add_argument('--bsize', action="store", dest="bs", default=16)
+parser.add_argument('--bsize', action="store", dest="bs", default=2)
 args = parser.parse_args()
 epochs = args.epochs
 batch_size = args.bs
 
-env = 1
-PATH = '/home/momi/Documents/599/Final/Code/densenet/' if env == 1 else '/home/mkhan31/ECE599_HW/Final/Code/densenet/' 
+home_dir = os.environ['HOME']
+if 'momi' in home_dir:
+    PATH = '/home/momi/Documents/599/Final/Code/densenet/'
+elif 'mkhan31' in home_dir:
+    PATH = '/home/mkhan31/ECE599_HW/Final/Code/densenet/'
+
 models_dir = PATH + 'models/'
-log_dir = PATH +'logs/'  
+log_dir = PATH + 'logs/'
 try:
     os.mkdir(models_dir)
     os.mkdir(log_dir)
 except OSError:
     print "Directories already created"
-    
+  
+# =============================================================================
+# Data loading and defining generator
+# =============================================================================
+with np.load('data.npz') as data:
+    # Training data
+    X_train = data['X_train'][:1000]
+    Y_train = data['Y_train'][:1000]
 
-ckpt = ModelCheckpoint(models_dir + 'weights.{epoch:02d}-{val_loss:.2f}.hdf5',
-                       verbose=1, period=5)
-early_stop = EarlyStopping(min_delta=0, patience=15, verbose=1)
-csvlog = CSVLogger(PATH + 'stats.csv', append=True)
-reducelr = ReduceLROnPlateau(verbose=1)
+    # Validation data
+    X_valid = data['X_valid']
+    Y_valid = data['Y_valid']
 
-# Load our model
-model = densenet(img_rows=224, img_cols=224, color_type=1, 
-                 num_classes=2, bn_type='bn', opt='adam')
-
-
-X_train, X_test, Y_train, Y_test = train_test_split(np.load('x.npy'),
-                                                      np.load('y.npy'),
-                                                      test_size=0.3,
-                                                      random_state=10)
-X_valid, Y_valid = X_train[800:1000,:,:, None], Y_train[800:1000,]
-X_train, Y_train = X_train[:800,:,:, None], Y_train[:800,]
-X_test = X_test[:,:,:, None]
-
-datagen = ImageDataGenerator(featurewise_center=True,
+train_gen = ImageDataGenerator(featurewise_center=True,
                              featurewise_std_normalization=True,
                              horizontal_flip=True,
                              zoom_range=0.2)
+validation_gen = ImageDataGenerator(featurewise_center=True,
+                                    featurewise_std_normalization=True)
+train_gen.fit(X_train)
+validation_gen.fit(X_valid)
 
-datagen.fit(X_train)
-model.fit_generator(datagen.flow(X_train, Y_train, batch_size=2),
-                    steps_per_epoch=len(X_train) / 2,  
+# =============================================================================
+# Define callbacks
+# =============================================================================
+ckpt = ModelCheckpoint(models_dir + 'weights.{epoch:02d}-{val_loss:.2f}.hdf5',
+                       verbose=1, period=5)
+early_stop = EarlyStopping(min_delta=0, patience=10, verbose=1)
+csvlog = CSVLogger(PATH + 'stats.csv', append=True)
+reducelr = ReduceLROnPlateau(verbose=1)
+
+# =============================================================================
+# Load and train model
+# =============================================================================
+
+model = densenet(img_rows=224, img_cols=224, color_type=1,
+                 num_classes=2, bn_type='brn', opt='adam')
+
+model.fit_generator(train_gen.flow(X_train, Y_train, batch_size=batch_size),
+                    steps_per_epoch=len(X_train) / batch_size,
                     epochs=1,
                     verbose=1,
-                    validation_data=(X_valid, Y_valid),
+                    validation_data=validation_gen.flow(X_valid, Y_valid, batch_size=batch_size),
+                    validation_steps=len(X_valid) / batch_size,
                     callbacks=[reducelr, ckpt, early_stop, csvlog])
 
-## Make predictions
+# Make predictions
 #datagen_y = ImageDataGenerator(featurewise_center=True,
 #                             featurewise_std_normalization=True)
 #datagen_y.fit(X_test)
@@ -71,5 +86,3 @@ model.fit_generator(datagen.flow(X_train, Y_train, batch_size=2),
 ## Cross-entropy loss score
 #print log_loss(Y_test[:800], predictions_valid)
 #
-
-
--- Project4/main.py
+++ Project4/main.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 import pickle
 
 flags = tf.app.flags
-flags.DEFINE_integer("epoch", 1, "Epoch to train [25]")
+flags.DEFINE_integer("epoch", 5, "Epoch to train [25]")
 flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
 flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
 flags.DEFINE_integer("train_size", np.inf, "The size of train images [np.inf]")
