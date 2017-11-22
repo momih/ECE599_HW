9b057fad8fded8b3d1ba052d22c8c9017fabacfc
Hashir <mohammadhashir94@gmail.com>
2017-11-22 16:58:52 -0500

    keras save

--- Final/Code/densenet/test.py
+++ Final/Code/densenet/test.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, CSVLogger
-from keras.preprocessing.image import ImageDataGenerator
+#from keras.preprocessing.image import ImageDataGenerator
 #from sklearn.metrics import accuracy_score, log_loss
 from densenet121 import densenet
 import tensorflow as tf
@@ -8,22 +8,16 @@ import numpy as np
 import os
 import argparse
 import time
-import pickle
 
-tf.logging.set_verbosity(tf.logging.DEBUG)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--epochs', action="store", dest="epochs", default=2)
+parser.add_argument('--epochs', action="store", dest="epochs", default=1)
 parser.add_argument('--bsize', action="store", dest="bs", default=2)
-parser.add_argument('--n', action="store", dest="n", default=32)
-parser.add_argument('--load', action="store", dest="load", default='weights_iter_1')
-parser.add_argument('--save', action="store", dest="save", default='weights_iter_1')
+parser.add_argument('--n', action="store", dest="n", default=10)
 args = parser.parse_args()
 epochs = int(args.epochs)
 batch_size = int(args.bs)
 n = int(args.n)
-load_weights_name = args.load
-save_weights_name = args.save
 
 home_dir = os.environ['HOME']
 if 'momi' in home_dir:
@@ -52,21 +46,21 @@ with np.load('data.npz') as data:
     X_valid = data['X_valid'][:n]
     Y_valid = data['Y_valid'][:n]
 
-train_gen = ImageDataGenerator(featurewise_center=True,
-                             featurewise_std_normalization=True,
-                             horizontal_flip=True,
-                             zoom_range=0.2)
-validation_gen = ImageDataGenerator(featurewise_center=True,
-                                    featurewise_std_normalization=True)
-train_gen.fit(X_train)
-validation_gen.fit(X_valid)
+#train_gen = ImageDataGenerator(featurewise_center=True,
+#                             featurewise_std_normalization=True,
+#                             horizontal_flip=True,
+#                             zoom_range=0.2)
+#validation_gen = ImageDataGenerator(featurewise_center=True,
+##                                    featurewise_std_normalization=True)
+#train_gen.fit(X_train)
+#validation_gen.fit(X_valid)
 
 # =============================================================================
 # Define callbacks
 # =============================================================================
 print('Define callbacks')
 ckpt = ModelCheckpoint(models_dir + 'weights.{epoch:02d}-{val_loss:.2f}.hdf5',
-                       verbose=1, period=5)
+                       verbose=1, period=1)
 early_stop = EarlyStopping(min_delta=0, patience=10, verbose=1)
 csvlog = CSVLogger(PATH + 'stats.csv', append=True)
 reducelr = ReduceLROnPlateau(verbose=1)
@@ -78,26 +72,13 @@ print('\nLoad model')
 
 model = densenet(img_rows=224, img_cols=224, color_type=1,
                  num_classes=2, bn_type='bn', opt='adam')
-pretrained_files = os.listdir('models/')
-if len(pretrained_files) > 0:
-    with open('models/' + load_weights_name, 'rb') as f:
-        loaded_weights = pickle.load(f)
-    model.set_weights(loaded_weights)
-    print('Loaded weights from models/' + load_weights_name)
-
-print('\n Train')    
+print('\n Train')
 start = time.time()
-model.fit_generator(train_gen.flow(X_train, Y_train, batch_size=batch_size),
-                    steps_per_epoch=len(X_train) / batch_size,
+model.fit(X_train, Y_train, batch_size=batch_size,
                     epochs=epochs,
                     verbose=1,
-                    validation_data=validation_gen.flow(X_valid, Y_valid, batch_size=batch_size),
-                    validation_steps=len(X_valid) / batch_size,
-                    callbacks=[reducelr, early_stop, csvlog])
-
-weights = model.get_weights()
-with open('models/' + save_weights_name, 'wb') as f:
-    pickle.dump(weights, f)
+                    validation_data=(X_valid, Y_valid),
+                    callbacks=[reducelr, ckpt, early_stop, csvlog])
 
 print(time.time() - start)
 # Make predictions
--- Final/Code/densenet/train.py
+++ Final/Code/densenet/train.py
@@ -3,15 +3,21 @@ from keras.preprocessing.image import ImageDataGenerator
 #from sklearn.metrics import accuracy_score, log_loss
 from densenet121 import densenet
 import numpy as np
+import pickle
 import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--epochs', action="store", dest="epochs", default=50)
-parser.add_argument('--bsize', action="store", dest="bs", default=8)
+parser.add_argument('--epochs', action="store", dest="epochs", default=10)
+parser.add_argument('--bsize', action="store", dest="bs", default=4)
+parser.add_argument('--load', action="store", dest="load", default='weights_iter_1')
+parser.add_argument('--save', action="store", dest="save", default='weights_iter_2')
+
 args = parser.parse_args()
 epochs = int(args.epochs)
 batch_size = int(args.bs)
+load_weights_name = args.load
+save_weights_name = args.save
 
 home_dir = os.environ['HOME']
 if 'momi' in home_dir:
@@ -51,18 +57,24 @@ validation_gen.fit(X_valid)
 # =============================================================================
 # Define callbacks
 # =============================================================================
-ckpt = ModelCheckpoint(models_dir + 'weights.{epoch:02d}-{val_loss:.2f}.hdf5',
-                       verbose=1, period=5)
-early_stop = EarlyStopping(min_delta=0, patience=10, verbose=1)
-csvlog = CSVLogger(PATH + 'stats.csv', append=True)
-reducelr = ReduceLROnPlateau(verbose=1)
+early_stop = EarlyStopping(min_delta=0, patience=5, verbose=1)
+csvlog = CSVLogger(models_dir + save_weights_name +'_stats.csv', append=True)
+reducelr = ReduceLROnPlateau(verbose=1, patience=3)
 
 # =============================================================================
 # Load and train model
 # =============================================================================
 
 model = densenet(img_rows=224, img_cols=224, color_type=1,
-                 num_classes=2, bn_type='brn', opt='adam')
+                 num_classes=2, bn_type='bn', opt='adam')
+
+pretrained_files = os.listdir('models/')
+if len(pretrained_files) > 0:
+    with open(models_dir + load_weights_name, 'rb') as f:
+        loaded_weights = pickle.load(f)
+    model.set_weights(loaded_weights)
+    print('Loaded weights from ' + models_dir + load_weights_name)
+    
 
 model.fit_generator(train_gen.flow(X_train, Y_train, batch_size=batch_size),
                     steps_per_epoch=len(X_train) / batch_size,
@@ -70,19 +82,11 @@ model.fit_generator(train_gen.flow(X_train, Y_train, batch_size=batch_size),
                     verbose=1,
                     validation_data=validation_gen.flow(X_valid, Y_valid, batch_size=batch_size),
                     validation_steps=len(X_valid) / batch_size,
-                    callbacks=[reducelr, ckpt, early_stop, csvlog])
+                    callbacks=[reducelr, early_stop, csvlog])
 
-# Make predictions
-#datagen_y = ImageDataGenerator(featurewise_center=True,
-#                             featurewise_std_normalization=True)
-#datagen_y.fit(X_test)
-#predictions_valid = model.predict_generator(datagen_y.flow(X_test[:800], batch_size=batch_size),
-#                                            steps =len(X_test[:800])/batch_size,
-#                                            verbose=1)
-#r=X_test[:800]
-#r =  (r - r.mean(axis=0))/r.std(axis=0)
-#
-#w = model.predict(r, batch_size=16, verbose=1)
-## Cross-entropy loss score
-#print log_loss(Y_test[:800], predictions_valid)
-#
+
+weights = model.get_weights()
+with open(models_dir + save_weights_name, 'wb') as f:
+    pickle.dump(weights, f)
+
+print('Saved weights to ' + models_dir + save_weights_name)
